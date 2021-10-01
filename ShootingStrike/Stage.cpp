4817c2b2ba541f0b1c554833b60ff5c1c6e8f057
OneBarrack <wnsdd1@gmail.com>
2021-10-01 17:10:38 +0900

    * Stage BoundaryCollision 체크 코드 삭제

--- ShootingStrike/Stage.cpp
+++ ShootingStrike/Stage.cpp
@@ -86,11 +86,6 @@ void Stage::Initialize()
 void Stage::Update()
 {
 	ObjectManager::GetInstance()->Update();
-
-	// ** 해당 오브젝트들이 메인 Background 내부를 벗어났는지 체크
-	//CheckPositionInBkgBoundary(eObjectKey::PLAYER);
-	//CheckPositionInBkgBoundary(eObjectKey::ENEMY);
-	//CheckPositionInBkgBoundary(eObjectKey::BULLET);
 }
 
 void Stage::Render(HDC _hdc)
@@ -105,66 +100,4 @@ void Stage::Release()
 	if ( pRightSideBackground ) ObjectManager::GetInstance()->RecallObject(pRightSideBackground);
 	if ( pScoreUI )				ObjectManager::GetInstance()->RecallObject(pScoreUI);
 	if ( pBossAngelEnemy )		ObjectManager::GetInstance()->RecallObject(pBossAngelEnemy);
-}
-
-void Stage::CheckPositionInBkgBoundary(eObjectKey _ObjectKey)
-{
-	// ** Stage의 바운더리
-	RectF ScreenRect = pBackground->GetColliderF();
-
-	// ** Key에 해당하는 Object List를 받아옴
-	list<Object*> ObjectList = ObjectManager::GetInstance()->GetObjectList(_ObjectKey);
-
-	// ** List를 돌며 Position 체크
-	for ( Object* pObject : ObjectList )
-	{
-		// ** Object의 Position과 
-		Vector3 ObjectPosition = pObject->GetPosition();		
-
-		float Offset = 0.0f;
-		switch ( pObject->GetKey() )
-		{
-			case eObjectKey::PLAYER:
-				// ** 테두리 경계선 기준에 몸체가 잘리지 않게 하기 위한 Offset 값
-				Offset = 22.0f;
-
-				// ** 좌
-				if ( ObjectPosition.x < ScreenRect.Left + Offset )
-					ObjectPosition.x = ScreenRect.Left + Offset;
-				// ** 우
-				if ( ObjectPosition.x > ScreenRect.Right - Offset )
-					ObjectPosition.x = ScreenRect.Right - Offset;
-				// ** 상
-				if ( ObjectPosition.y < ScreenRect.Top + Offset )
-					ObjectPosition.y = ScreenRect.Top + Offset;
-				// ** 하
-				if ( ObjectPosition.y > ScreenRect.Bottom - Offset )
-					ObjectPosition.y = ScreenRect.Bottom - Offset;
-
-				// ** 계산된 Position을 다시 플레이어에 세팅
-				pObject->SetPosition(ObjectPosition);
-
-				break;
-			case eObjectKey::ENEMY:
-				[[fallthrough]];
-			case eObjectKey::BULLET:
-				// ** Stage 바운더리를 넓힐 여유분 길이의 Offset
-				Offset = 0.0f;
-
-				// ** Stage 바운더리를 Offset 만큼 넓힌다
-				ScreenRect.Left   -= Offset;
-				ScreenRect.Top    -= Offset;
-				ScreenRect.Right  += Offset;
-				ScreenRect.Bottom += Offset;
-
-				// ** Stage의 바운더리 내 Object Position이 위치하지 않으면 Destroy
-				if ( !CollisionManager::IsPointInRect(ScreenRect, ObjectPosition) )
-				{
-					pObject->SetStatus(eObjectStatus::DESTROYED);
-				}
-				break;
-			default:
-				break;
-		}
-	}
 }
--- ShootingStrike/Stage.h
+++ ShootingStrike/Stage.h
@@ -20,10 +20,6 @@ public:
 	virtual void Render(HDC _hdc)override;
 	virtual void Release()override;
 
-private:
-	// ** 오브젝트들이 메인 Background를 벗어났는지 체크
-	void CheckPositionInBkgBoundary(eObjectKey _ObjectKey);
-
 public:
 	Stage();
 	virtual ~Stage();
